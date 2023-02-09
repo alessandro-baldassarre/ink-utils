bf35d1b47072d1e9f894f39305be6a3102ec1d73
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-02-09 22:12:14 +0100

    Update ink 4.0.0rc:delete AccountId Default implementation

--- traits/ink-group/src/storage.rs
+++ traits/ink-group/src/storage.rs
@@ -1,7 +1,7 @@
 use ink::primitives::AccountId;
 use scale::{Decode, Encode};
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Ord, PartialOrd, Hash, Encode, Decode, Default)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Ord, PartialOrd, Hash, Encode, Decode)]
 #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
 /// Member of the group
 pub struct Member {
